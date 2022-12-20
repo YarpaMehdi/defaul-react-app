b1585c25277ce890bf2b685cc16b283ea5a14914
Yarpa <yarpamehdi2000@gmail.com>
2022-12-21 02:55:14 +0500

    updated react default app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import { FcAcceptDatabase } from "react-icons/fc";
+
+const App = () => {
+  return (
+    <>
+      {/* <Router>
+        <Routes>
+          <Route path='/' element={<Login />}/>
+          <Route path='/fromu' element={<Fromu />}/>
+        </Routes>
+      </Router> */}
+    </>
+  )
+}
+
+export default App
